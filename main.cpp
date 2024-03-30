0e3ad65ca0b36963d064904d3bdaed00dfb38946
BrainDBD <BrainDBD@users.noreply.github.com>
2024-03-30 22:29:26 +0200

    Added a number of important classes and their functions for later.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,43 @@
 #include <iostream>
-#include <array>
+#include <vector>
+#include <algorithm>
+#include <random>
+class Card{
+private:
+    int value;
+public:
 
-#include <Helper.h>
+    Card();
+    Card(const int value_) : value{value_} {}
+    int getValue() const { return value; }
+    friend std::ostream& operator<<(std::ostream& os, const Card& card) {
+        os << "Cardul are valoarea: "<< card.value << "\n";
+        return os;
+    }
+    ~Card() = default;
+};
+class Deck{
+private:
+    std::vector<Card> cards;
+public:
+    Deck();
+    void shuffle(){};
+    ~Deck() = default;
+};
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+class TrumpCard{
+
+};
+class TrumpDeck: public Deck{
+private:
+    std::vector<TrumpCard> tcards;
+public:
+    TrumpDeck();
+    ~TrumpDeck() = default;
+};
+int main()
+{
+    Card card{2};
+    std::cout<<card;
     return 0;
 }
