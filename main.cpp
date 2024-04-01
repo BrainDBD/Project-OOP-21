5d9a222e2260f8312b771062988733639acc7567
BrainDBD <BrainDBD@users.noreply.github.com>
2024-04-01 13:48:55 +0300

    Added add and shuffle functions to Deck, overloaded << and = for Deck, analogously for TrumpDeck (WIP), and = for Card

--- main.cpp
+++ main.cpp
@@ -13,8 +13,13 @@ public:
     int getValue() const { return rank; }
     friend std::ostream &operator<<(std::ostream &os, const Card &card)
     {
-        os << "Cardul are valoarea: " << card.rank << '\n';
+        os << card.rank << " ";
         return os;
+    }
+    Card &operator=(const Card &other)
+    {
+        rank = other.rank;
+        return *this;
     }
     ~Card() = default;
 };
@@ -24,21 +29,67 @@ private:
     std::vector<Card> cards;
 
 public:
-    Deck();
+    void addToDeck(Card card_)
+    {
+        cards.push_back(card_);
+    }
+    /*Deck(vector<Card> cards_): cards{cards_}{}*/
+    void shuffleDeck()
+    {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        shuffle(cards.begin(), cards.end(), g);
+    }
     ~Deck() = default;
+    friend std::ostream &operator<<(std::ostream &os, const Deck &deck)
+    {
+        for (unsigned int i = 0; i < deck.cards.size(); i++)
+            std::cout << deck.cards[i] << " ";
+        return os;
+    }
+    Deck &operator=(const Deck &other)
+    {
+        for (unsigned int i = 0; i < other.cards.size(); i++)
+            cards[i] = other.cards[i];
+        return *this;
+    }
 };
 
-class TrumpCard{
+class TrumpCard
+{
 private:
     std::string type;
 };
-class TrumpDeck{
+class TrumpDeck
+{
 private:
     std::vector<TrumpCard> tcards;
 
 public:
-    TrumpDeck();
+    void addToDeck(TrumpCard tcard_)
+    {
+        tcards.push_back(tcard_);
+    }
+    /*Deck(vector<Card> cards_): cards{cards_}{}*/
+    void shuffleDeck()
+    {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        shuffle(tcards.begin(), tcards.end(), g);
+    }
     ~TrumpDeck() = default;
+    /*friend std::ostream &operator<<(std::ostream &os, const TrumpDeck &tdeck)
+    {
+        for (unsigned int i = 0; i < tdeck.tcards.size(); i++)
+            std::cout << tdeck.tcards[i] << " ";
+        return os;
+    }*/
+    TrumpDeck &operator=(const TrumpDeck &other)
+    {
+        for (unsigned int i = 0; i < other.tcards.size(); i++)
+            tcards[i] = other.tcards[i];
+        return *this;
+    }
 };
 class Player
 {
@@ -60,7 +111,8 @@ public:
         }
         return *this;
     }
-    friend std::ostream& operator<<(std::ostream& os, const Player& player) {
+    friend std::ostream &operator<<(std::ostream &os, const Player &player)
+    {
         os << "Name: " << player.name << std::endl;
         os << "Score: " << player.score << std::endl;
         os << "Health: " << player.hp << std::endl;
@@ -71,7 +123,11 @@ public:
 };
 int main()
 {
-    Card card1{1}, card2{2}, card3{3}, card4{4}, card5{5}, card6{6}, card7{7}, card8{8}, card9{9}, card10{10}, card11{11};
-    std::cout << card2 << " " << card4;
+    Card vcard[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    Deck deck1;
+    for (int i = 0; i < 11; i++)
+        deck1.addToDeck(vcard[i]);
+    deck1.shuffleDeck();
+    std::cout << deck1;
     return 0;
 }
